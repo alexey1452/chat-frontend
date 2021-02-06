54a26f0a6e52c775d3c671528920774a1073bfaf
alexey1452 <kirichenko1452@gmail.com>
2021-02-06 18:20:54 +0200

    show username

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {Store} from '@ngrx/store';
 import { IAppStore } from '../../store/app.store';
 import { Logout } from '../../store/user/user.actions';
+import { IUser } from '../../interfaces/user.interface';
+import { getAuthUser } from '../../store/user/user.reducer';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,11 +12,18 @@ import { Logout } from '../../store/user/user.actions';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  authUser$: Observable<IUser>;
+  username: string;
 
-  constructor(private store: Store<IAppStore>) { }
-
-  ngOnInit(): void {
+  constructor(private store: Store<IAppStore>) {
+      this.username = '';
+      this.authUser$ = this.store.select(getAuthUser);
+      this.authUser$.subscribe(user => {
+          this.username = user.firstName + ' ' + user.lastName;
+      });
   }
+
+  ngOnInit(): void { }
 
   logout(): void {
       this.store.dispatch(new Logout());
